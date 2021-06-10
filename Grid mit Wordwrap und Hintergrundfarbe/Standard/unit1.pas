67c587700af180028439d4fc74fde43c4925729d
Alexander Johr <alexander.johr@thuenen.de>
2021-06-10 17:18:37 +0200

    barebone project

---
+++ Grid mit Wordwrap und Hintergrundfarbe/Standard/unit1.pas
@@ -0,0 +1,29 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, DB, dbf, Forms, Controls, Graphics, Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
