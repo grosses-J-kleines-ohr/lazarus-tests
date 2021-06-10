25e9355dfcd29726e328e5bba9a2cdbcebecb8a1
Alexander Johr <alexander.johr@thuenen.de>
2021-06-10 17:40:49 +0200

    added simple grid

--- Grid mit Wordwrap und Hintergrundfarbe/Standard/unit1.pas
+++ Grid mit Wordwrap und Hintergrundfarbe/Standard/unit1.pas
@@ -5,13 +5,20 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, DB, dbf, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, DB, dbf, PQConnection, SQLDB, Forms, Controls, Graphics,
+  Dialogs, DBGrids;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    DataSource1: TDataSource;
+    DBGrid1: TDBGrid;
+    PQConnection1: TPQConnection;
+    SQLQuery1: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -24,6 +31,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  SQLQuery1.SQL.Text := 'select * from books';
+end;
 
 end.
 
